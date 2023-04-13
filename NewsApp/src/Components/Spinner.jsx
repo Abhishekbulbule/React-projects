e67edf99265f67f939afc9444bdb3de6a197b0bb
Abhishek <78312498+Abhishekbulbule@users.noreply.github.com>
2023-04-13 13:54:59 +0530

    Some addition

---
+++ NewsApp/src/Components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="text-center m-3 ">
+      <div className="spinner-border " role="status"></div>
+    </div>
+  );
+};
+
+export default Spinner;
